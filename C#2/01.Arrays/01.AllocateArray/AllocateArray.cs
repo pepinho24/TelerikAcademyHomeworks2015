2965aa7e9e3953ed5d29cfe4c0209bba46db6cf0
pepinho24 <p_mil4ev@hotmail.com>
2015-02-11 22:51:14 +0200

    Initial Commit

---
+++ C#2/01.Arrays/01.AllocateArray/AllocateArray.cs
@@ -0,0 +1,18 @@
+﻿//Problem 1. Allocate array
+
+//Write a program that allocates array of 20 integers and initializes each element by its index multiplied by 5.
+//Print the obtained array on the console.
+namespace _01.AllocateArray
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class AllocateArray
+    {
+        static void Main()
+        {
+        }
+    }
+}

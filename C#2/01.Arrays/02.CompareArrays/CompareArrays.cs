865233c88526d2271ddc63a370ab9008a6d935ab
pepinho24 <p_mil4ev@hotmail.com>
2015-02-11 23:24:15 +0200

    Finished task 2 - Compare arrays

---
+++ C#2/01.Arrays/02.CompareArrays/CompareArrays.cs
@@ -0,0 +1,70 @@
+﻿//Problem 2. Compare arrays
+
+//Write a program that reads two integer arrays from the console and compares them element by element.
+namespace _02.CompareArrays
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CompareArrays
+    {
+        private const string DIFFERENT_ARRAYS_MESSAGE = "The arrays are different!";
+        private const string EQUAL_ARRAYS_MESSAGE = "The arrays are equal!";
+        public static void Main()
+        {
+            Console.Write("Please enter the elements of the first array separated by space: ");
+            int[] firstArray = ReadArray();
+
+            Console.Write("Please enter the elements of the second array separated by space: ");
+            int[] secondArray = ReadArray();
+
+            if (firstArray.Length != secondArray.Length)
+            {
+                Console.WriteLine(DIFFERENT_ARRAYS_MESSAGE);
+            }
+            else
+            {
+                for (int i = 0; i < firstArray.Length; i++)
+                {
+                    if (firstArray[i]!= secondArray[i])
+                    {
+                        Console.WriteLine(DIFFERENT_ARRAYS_MESSAGE);
+                        return;
+                    }
+                }
+                Console.WriteLine(EQUAL_ARRAYS_MESSAGE);
+            }
+
+        }
+
+        private static int[] ReadArray()
+        {
+            string arrayAsString = Console.ReadLine();
+            // Using LINQ
+            string[] arrayOfStrings = arrayAsString.Split(' ');
+            int[] arrayOfIntegers = new int[arrayOfStrings.Length];
+
+            for (int i = 0; i < arrayOfStrings.Length; i++)
+            {
+                try
+                {
+                    arrayOfIntegers[i] = int.Parse(arrayOfStrings[i]);
+                }
+                catch (FormatException)
+                {
+                    throw new FormatException("Input string was not in the correct format!");
+                }
+                
+            }
+
+            return arrayOfIntegers;
+        }
+        private static void PrintArray(int[] array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}

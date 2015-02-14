550ecc8c08d936b91ecde79b6b64e7f90429b924
pepinho24 <p_mil4ev@hotmail.com>
2015-02-14 20:59:40 +0200

    Solved task 4 - Binary search

--- C#2/02.MultidimensionalArrays/04.BinarySearch/BinarySearch.cs
+++ C#2/02.MultidimensionalArrays/04.BinarySearch/BinarySearch.cs
@@ -9,15 +9,47 @@
 namespace _04.BinarySearch
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class BinarySearch
     {
         public static void Main()
         {
+            Console.Write("Enter N: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter K: ");
+            int k = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter integer {0}: ", i + 1);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(arr);
+            int index = Array.BinarySearch(arr, k);
+
+            if (index < 0)
+            {
+                // If the index is negative, it represents the bitwise 
+                // complement of the next larger element in the array. 
+                index = ~index;
+
+                if (index == 0)
+                {
+                    Console.WriteLine("There is no element smaller or equal to {0}", k);
+                }
+                else
+                {
+                    Console.WriteLine("The biggest element smaller of {0} is {1}", k, arr[index - 1]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The biggest element smaller or equal to {0} is {1}", k, arr[index]);
+            }
         }
     }
 }

9138bb94d7ba976b23e5bd7710bca1c2c751905d
pepinho24 <p_mil4ev@hotmail.com>
2015-02-17 22:14:37 +0200

    Solved task 7 - Reverse number

--- C#2/03.Methods/07.ReverseNumber/ReverseNumber.cs
+++ C#2/03.Methods/07.ReverseNumber/ReverseNumber.cs
@@ -6,14 +6,36 @@ Example:
 input	output
 256	    652*/
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 public class ReverseNumber
 {
     public static void Main()
     {
+        decimal number = -123.45M;
+        decimal reversedNumber = ReturnReversedNumber(number);
+
+        Console.WriteLine(reversedNumber);
+    }
+
+    private static decimal ReturnReversedNumber(decimal number)
+    {
+        decimal reversedNumber = number;
+      
+        var numberToChar = number.ToString().ToArray();
+
+        if (numberToChar[0]== '-')
+        {
+            var temp = numberToChar.Skip(1);
+            var num = temp.ToArray().Reverse().ToArray();
+            reversedNumber = - decimal.Parse(string.Join("", num));
+        }
+        else
+        {
+            var num = numberToChar.ToArray().Reverse().ToArray();
+            reversedNumber = decimal.Parse(string.Join("",num));
+        }
+
+        return reversedNumber;
     }
 }

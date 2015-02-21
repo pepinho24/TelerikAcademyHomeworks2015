8eaad641d1d14d33c895bb9e24e23a76e77521af
pepinho24 <p_mil4ev@hotmail.com>
2015-02-21 21:25:42 +0200

    Solved task 11 - Adding polynomials

--- C#2/03.Methods/11.AddingPolynomials/AddingPolynomials.cs
+++ C#2/03.Methods/11.AddingPolynomials/AddingPolynomials.cs
@@ -6,14 +6,86 @@ Example:
 
 x2 + 5 = 1x2 + 0x + 5 => {5, 0, 1}*/
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 public class AddingPolynomials
 {
-    public static void Main()
+    private static decimal[] SumOfPolinomials(decimal[] firstPolinomial, decimal[] secondPolinomial)
     {
+        int minLenght = 0;
+        int smallerPolinomial = 0;
+        decimal[] result;
+
+        if (firstPolinomial.Length > secondPolinomial.Length)
+        {
+            result = new decimal[firstPolinomial.Length];
+            minLenght = secondPolinomial.Length;
+            smallerPolinomial = 2;
+        }
+        else
+        {
+            result = new decimal[secondPolinomial.Length];
+            minLenght = firstPolinomial.Length;
+            smallerPolinomial = 1;
+        }
+
+        for (int i = 0; i < minLenght; i++)
+        {
+            result[i] = firstPolinomial[i] + secondPolinomial[i];
+        }
+
+        for (int i = minLenght; i < result.Length; i++)
+        {
+            if (smallerPolinomial == 1)
+            {
+                result[i] = secondPolinomial[i];
+            }
+            else
+            {
+                result[i] = firstPolinomial[i];
+            }
+        }
+
+        return result;
+    }
+
+    private static void PrintPolinomial(decimal[] polinomial)
+    {
+        for (int i = polinomial.Length - 1; i >= 0; i--)
+        {
+            if (polinomial[i] != 0 && i != 0)
+            {
+                if (polinomial[i - 1] >= 0)
+                {
+                    Console.Write("{1}x^{0} +", i, polinomial[i]);
+                }
+                else
+                {
+                    Console.Write("{1}x^{0} ", i, polinomial[i]);
+                }
+            }
+            else if (i == 0)
+            {
+                Console.Write("{0}", polinomial[i]);
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        decimal[] firstPolinomial = { 5, -1 };
+        Console.Write("First polinomial:                 ");
+        PrintPolinomial(firstPolinomial);
+
+        decimal[] secondPolinomial = { 10, -5, 6 };
+        Console.Write("Second polinomial:                ");
+        PrintPolinomial(secondPolinomial);
+
+        Console.WriteLine();
+
+        decimal[] result = SumOfPolinomials(firstPolinomial, secondPolinomial);
+        Console.Write("Sum:                              ");
+        PrintPolinomial(result);
     }
 }

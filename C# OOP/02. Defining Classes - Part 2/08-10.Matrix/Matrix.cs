6c0963dd0d146151038b9ea240bfde8c0975af7c
pepinho24 <p_mil4ev@hotmail.com>
2015-03-16 00:27:52 +0200

    Solved the tasks for the matrices + added testing in the main method

---
+++ C# OOP/02. Defining Classes - Part 2/08-10.Matrix/Matrix.cs
@@ -0,0 +1,192 @@
+﻿/*
+ Problem 8. Matrix
+
+        Define a class Matrix<T> to hold a matrix of numbers (e.g. integers, floats, decimals).
+
+ * Problem 9. Matrix indexer
+
+        Implement an indexer this[row, col] to access the inner matrix cells.
+
+ * Problem 10. Matrix operations
+
+        Implement the operators + and - (addition and subtraction of matrices of the same size) and * for matrix multiplication.
+        Throw an exception when the operation cannot be performed.
+        Implement the true operator (check for non-zero elements).
+ */
+namespace _08_10.Matrix
+{
+    using System;
+    using System.Text;
+    using System.Linq;
+
+    public class Matrix<T>
+        where T : IComparable
+    {
+        private int rows;
+        private int cols;
+        private T[,] matrix;
+
+        public Matrix(int rowsCount, int colsCount)
+        {
+            this.Rows = rowsCount;
+            this.Cols = colsCount;
+            this.matrix = new T[rowsCount, colsCount];
+        }
+
+        public T this[int row, int col]
+        {
+            get
+            {
+                CheckIndexes(row, col);
+                return this.matrix[row, col];
+            }
+            set
+            {
+                CheckIndexes(row, col);
+                this.matrix[row, col] = value;
+            }
+        }
+
+        private void CheckIndexes(int row, int col)
+        {
+            if ((row < 0 || row >= this.Rows) ||
+                (col < 0 || col >= this.Cols))
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public int Rows
+        {
+            get { return this.rows; }
+            private set
+            {
+                if (value < 1) { throw new ArgumentOutOfRangeException("Rows Count", "The matrix must have at least one row!"); }
+
+                this.rows = value;
+            }
+        }
+
+        public int Cols
+        {
+            get { return this.cols; }
+            private set
+            {
+                if (value < 1) { throw new ArgumentOutOfRangeException("Columns Count", "The matrix must have at least one column!"); }
+
+                this.cols = value;
+            }
+        }
+
+        public static Matrix<T> operator +(Matrix<T> firstMatrix, Matrix<T> secondMatrix)
+        {
+            if (firstMatrix.Rows != secondMatrix.Rows|| firstMatrix.Cols != secondMatrix.Cols )
+            {
+                throw new Exception("Addition cannot be applied to matrices with different dimensions!");
+            }
+
+            Matrix<T> resultMatrix = new Matrix<T>(firstMatrix.Rows, firstMatrix.Cols);
+
+            for (int row = 0; row < resultMatrix.Rows; row++)
+            {
+                for (int col = 0; col < resultMatrix.Cols; col++)
+                {
+                    // http://stackoverflow.com/questions/8122611/c-sharp-adding-two-generic-values
+                    resultMatrix[row, col] = (dynamic)firstMatrix[row, col] + secondMatrix[row, col];
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        public static Matrix<T> operator -(Matrix<T> firstMatrix, Matrix<T> secondMatrix)
+        {
+            if (firstMatrix.Rows != secondMatrix.Rows || firstMatrix.Cols != secondMatrix.Cols)
+            {
+                throw new Exception("Substraction cannot be applied to matrices with different dimensions!");
+            }
+
+            Matrix<T> resultMatrix = new Matrix<T>(firstMatrix.Rows, firstMatrix.Cols);
+
+            for (int row = 0; row < resultMatrix.Rows; row++)
+            {
+                for (int col = 0; col < resultMatrix.Cols; col++)
+                {
+                    // http://stackoverflow.com/questions/8122611/c-sharp-adding-two-generic-values
+                    resultMatrix[row, col] = (dynamic)firstMatrix[row, col] - secondMatrix[row, col];
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        public static Matrix<T> operator *(Matrix<T> firstMatrix, Matrix<T> secondMatrix)
+        {
+            // http://www.mathwarehouse.com/algebra/matrix/multiply-matrix.php
+            if (firstMatrix.Cols != secondMatrix.Rows)
+            {
+                throw new Exception("The matrices cannot be multiplied!");
+            }
+
+            Matrix<T> resultMatrix = new Matrix<T>(firstMatrix.Rows, secondMatrix.Cols);
+            dynamic temp;
+
+            for (int matrixRow = 0; matrixRow < resultMatrix.Rows; matrixRow++)
+            {
+                for (int matrixCol = 0; matrixCol < resultMatrix.Cols; matrixCol++)
+                {
+                    temp = 0;
+                    for (int index = 0; index < secondMatrix.Rows; index++)
+                    {
+                        temp += (dynamic)firstMatrix[matrixRow, index] * secondMatrix[index, matrixCol];
+                    }
+
+                    resultMatrix[matrixRow, matrixCol] = temp;
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        private static bool OverrideBool(Matrix<T> matrix)
+        {
+            for (int row = 0; row < matrix.Rows; row++)
+            {
+                for (int col = 0; col < matrix.Cols; col++)
+                {
+                    if (matrix[row, col] != (dynamic)0)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool operator true(Matrix<T> matrix)
+        {
+            return OverrideBool(matrix);
+        }
+
+        public static bool operator false(Matrix<T> matrix)
+        {
+            return OverrideBool(matrix);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int row = 0; row < this.Rows; row++)
+            {
+                for (int col = 0; col < this.Cols; col++)
+                {
+                    result.Append(this.matrix[row, col] + "\t");
+                }
+
+                result.AppendLine();
+            }
+
+            return result.ToString();
+        }
+    }
+}
--- C# OOP/02. Defining Classes - Part 2/08-10.Matrix/StartingPointMatrix.cs
+++ C# OOP/02. Defining Classes - Part 2/08-10.Matrix/StartingPointMatrix.cs
@@ -11,20 +11,104 @@
 
         Implement the operators + and - (addition and subtraction of matrices of the same size) and * for matrix multiplication.
         Throw an exception when the operation cannot be performed.
-        Implement the true operator (check for non-zero elements).
+        Implement the true operator (check for non-zero elements). * I assume that if the matrix contains only zeros it should be true*
  */
 namespace _08_10.Matrix
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class StartingPointMatrix
     {
         public static void Main()
         {
+            Matrix<int> firstMatrix = new Matrix<int>(3, 3);
+            Matrix<int> secondMatrix = new Matrix<int>(3, 2);
+            
+            // the matrices are filled with the values from the last problem (Practice 2) from whe link below where you can check the result
+            // http://www.mathwarehouse.com/algebra/matrix/multiply-matrix.php
+            FillFirstMatrixForMultiplication(firstMatrix);
+            FillSecondMatrixForMultiplication(secondMatrix);
+
+            Console.WriteLine("Matrix A: ");
+            Console.WriteLine(firstMatrix);
+
+            Console.WriteLine("Matrix B: ");
+            Console.WriteLine(secondMatrix);
+
+            Matrix<int> multiplication = firstMatrix * secondMatrix;
+
+            Console.WriteLine("Matrix AxB: ");
+            Console.WriteLine(multiplication);
+
+            PrintSeparatorLine();
+
+            firstMatrix = new Matrix<int>(2, 2);
+            secondMatrix = new Matrix<int>(2, 2);
+
+            FillMatrixForAdditionAndSubstraction(firstMatrix);
+            FillMatrixForAdditionAndSubstraction(secondMatrix);
+
+            Console.WriteLine("Matrix A: ");
+            Console.WriteLine(firstMatrix);
+
+            Console.WriteLine("Matrix B: ");
+            Console.WriteLine(secondMatrix);
+            
+            Matrix<int> addition = firstMatrix + secondMatrix;
+            Console.WriteLine("Matrix A+B: ");
+            Console.WriteLine(addition);
+            
+            Matrix<int> substraction = firstMatrix - secondMatrix;
+            Console.WriteLine("Matrix A-B: ");
+            Console.WriteLine(substraction);
+
+            PrintSeparatorLine();
+
+            Console.WriteLine("Is the addition result only zeros? {0}", addition? "yes": "no");
+            Console.WriteLine("Is the substraction result only zeros? {0}", substraction ? "yes" : "no");
+        }
+
+        private static void PrintSeparatorLine()
+        {
+            Console.WriteLine(new string('=', Console.BufferWidth));
+        }
+
+        private static void FillMatrixForAdditionAndSubstraction(Matrix<int> matrix)
+        {
+            int[,] fMatrix = new int[,] { { 1, 2 }, { 3, 4 } };
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    matrix[row, col] = fMatrix[row, col];
+                }
+            }
+        }
+
+        private static void FillFirstMatrixForMultiplication(Matrix<int> firstMatrix)
+        {
+            int[,] fMatrix = new int[,] { { 6, 3, 0 }, { 2, 5, 1 }, { 9, 8, 6 } };
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    firstMatrix[row, col] = fMatrix[row, col];
+                }
+            }
+        }
+
+        private static void FillSecondMatrixForMultiplication(Matrix<int> secondMatrix)
+        {
+            int[,] sMatrix = new int[,] { { 7, 4 }, { 6, 7 }, { 5, 0 } };
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    secondMatrix[row, col] = sMatrix[row, col];
+                }
+            }
         }
     }
 }

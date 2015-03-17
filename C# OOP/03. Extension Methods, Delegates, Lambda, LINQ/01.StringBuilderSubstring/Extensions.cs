8d8f0b31d9ce00382f2d36e08f5d4086f9004ec7
pepinho24 <p_mil4ev@hotmail.com>
2015-03-17 23:21:48 +0200

    Refactored task 1

---
+++ C# OOP/03. Extension Methods, Delegates, Lambda, LINQ/01.StringBuilderSubstring/Extensions.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.StringBuilderSubstring
+{
+    using System.Text;
+
+    public static class Extensions
+    {
+        public static StringBuilder Substring(this StringBuilder str, int index, int length)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = index; i < index + length && i < str.Length; i++)
+            {
+                sb.Append(str[i]);
+            }
+
+            return sb;
+        }
+    }
+}
--- C# OOP/03. Extension Methods, Delegates, Lambda, LINQ/01.StringBuilderSubstring/StringBuilderSubstring.cs
+++ C# OOP/03. Extension Methods, Delegates, Lambda, LINQ/01.StringBuilderSubstring/StringBuilderSubstring.cs
@@ -5,24 +5,7 @@ and has the same functionality as Substring in the class String.*/
 namespace _01.StringBuilderSubstring
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;
-    public static class Extensions
-    {
-        public static StringBuilder Substring(this StringBuilder str, int index, int length)
-        {
-            var sb = new StringBuilder();
-
-            for (int i = index; i < index + length && i < str.Length; i++)
-            {
-                sb.Append(str[i]);
-            }
-
-            return sb;
-        }
-    }
 
     public class StringBuilderSubstring
     {

d1e3c272d80a620df18c5b1761848d34c1cc9a0e
pepinho24 <p_mil4ev@hotmail.com>
2015-06-21 15:43:55 +0300

    Added solved homework for the Code formatting lecture from the High Quality Course

---
+++ High Quality Code/02.CodeFormatting/02.CodeFormatting/Event.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+public class Event : IComparable
+{
+    private DateTime date;
+    private string title;
+    private string location;
+
+    public Event(DateTime date, string title, string location)
+    {
+        this.date = date;
+        this.title = title;
+        this.location = location;
+    }
+
+    public int CompareTo(object otherEvent)
+    {
+        Event other = otherEvent as Event;
+        int compareByDate = this.date.CompareTo(other.date);
+        int compareByTitle = this.title.CompareTo(other.title);
+
+        int compareByLocation = this.location.CompareTo(other.location);
+        if (compareByDate == 0)
+        {
+            if (compareByTitle == 0)
+            {
+                return compareByLocation;
+            }
+            else
+            {
+                return compareByTitle;
+            }
+        }
+        else
+        {
+            return compareByDate;
+        }
+    }
+
+    public override string ToString()
+    {
+        StringBuilder eventToString = new StringBuilder();
+        eventToString.Append(this.date.ToString("yyyy-MM-ddTHH:mm:ss"));
+        eventToString.Append(" | " + this.title);
+
+        if (this.location != null && this.location != string.Empty)
+        {
+            eventToString.Append(" | " + this.location);
+        }
+
+        return eventToString.ToString();
+    }
+}
---
+++ High Quality Code/02.CodeFormatting/02.CodeFormatting/EventHolder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Wintellect.PowerCollections;
+
+public class EventHolder
+{
+    private MultiDictionary<string, Event> eventsByTitle = new MultiDictionary<string, Event>(true);
+    private OrderedBag<Event> eventsByDate = new OrderedBag<Event>();
+
+    public void AddEvent(DateTime date, string title, string location)
+    {
+        Event newEvent = new Event(date, title, location);
+        this.eventsByTitle.Add(title.ToLower(), newEvent);
+        this.eventsByDate.Add(newEvent);
+
+        Messages.EventAdded();
+    }
+
+    public void DeleteEvents(string titleToDelete)
+    {
+        string title = titleToDelete.ToLower();
+        int removedEventsCount = 0;
+
+        foreach (var eventToRemove in this.eventsByTitle[title])
+        {
+            removedEventsCount++;
+            this.eventsByDate.Remove(eventToRemove);
+        }
+
+        this.eventsByTitle.Remove(title);
+        Messages.EventDeleted(removedEventsCount);
+    }
+
+    public void ListEvents(DateTime date, int count)
+    {
+        OrderedBag<Event>.View eventsToShow = this.eventsByDate.RangeFrom(new Event(date, string.Empty, string.Empty), true);
+
+        int showedEvents = 0;
+        foreach (var eventToShow in eventsToShow)
+        {
+            if (showedEvents == count)
+            {
+                break;
+            }
+
+            Messages.PrintEvent(eventToShow);
+            showedEvents++;
+        }
+
+        if (showedEvents == 0)
+        {
+            Messages.NoEventsFound();
+        }
+    }
+}

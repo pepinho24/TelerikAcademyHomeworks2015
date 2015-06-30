14bdadfe10aa7e4595dd6499318e0afbad23df98
pepinho24 <p_mil4ev@hotmail.com>
2015-06-30 17:22:50 +0300

    Solve the task template for prototypal inheritance homework

--- JavaScript OOP/05.Prototypal Inheritance/task-1.js
+++ JavaScript OOP/05.Prototypal Inheritance/task-1.js
@@ -5,12 +5,11 @@
  * method init() that gets the domElement type
  * i.e. `Object.create(domElement).init('div')`
  * property type that is the type of the domElement
- * a valid type is any non-empty string that contains only Latin letters and digits
+ * //a valid type is any non-empty string that contains only Latin letters and digits
  * property innerHTML of type string
  * gets the domElement, parsed as valid HTML
  * <type attr1="value1" attr2="value2" ...> .. content / children's.innerHTML .. </type>
- * property content of type string
- * sets the content of the element
+ * property content of type string sets the content of the element
  * works only if there are no children
  * property attributes
  * each attribute has name and value
@@ -62,20 +61,153 @@
 
 
 function solve() {
+
+    function isString(str) {
+        return typeof str == "string" ||
+            (typeof str == "object" && str.constructor === String);
+    }
+
+    function isEmptyObject(obj) {
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function isValidType(type) {
+        return type.length &&
+            isString(type) &&
+            /^[A-Z\d]+$/i.test(type);
+    }
+
+    function isValidAttribute(attribute) {
+        return attribute.length &&
+            isString(attribute) &&
+            /^[A-Z\d\-]+$/i.test(attribute);
+    }
+
     var domElement = (function () {
         var domElement = {
-            init: function(type) {
+            // Defining properties
+            get type() {
+                return this._type;
             },
-            appendChild: function(child) {
+            set type(value) {
+                this._type = value;
             },
-            addAttribute: function(name, value) {
+            get content() {
+                return this._content;
             },
-            get innerHTML(){
+            set content(value) {
+                this._content = value;
+            },
+            get attributes() {
+                return this._attributes;
+            },
+            set attributes(value) {
+                this._attributes = value;
+            },
+            get children() {
+                return this._children;
+            },
+            set children(value) {
+                this._children = value;
+            },
+            get parent() {
+                return this._parent;
+            },
+            set parent(value) {
+                this._parent = value;
+            },
+            init: function (type) {
+                if (!isValidType(type)) {
+                    throw new Error('Invalid type!');
+                }
 
+                this.type = type;
+                this.content = '';
+                this.attributes = [];
+                this.children = [];
+                this.parent;
+
+                return this;
+            },
+            appendChild: function (child) {
+                this.children.push(child);
+
+                if (!isString(child)) {
+                    child.parent = this;
+                }
+
+                return this;
+            },
+            addAttribute: function (name, value) {
+                if (!isValidAttribute(name)) {
+                    throw new Error('Invalid attribute!');
+                }
+
+                this.attributes[name] = value;
+
+                return this;
+            },
+            removeAttribute: function (attribute) {
+                if (!(isValidAttribute(attribute) && this.attributes[attribute])) {
+                    throw new Error('Invalid attribute!');
+                }
+
+                delete this.attributes[attribute];
+
+                return this;
+            },
+            get innerHTML() {
+                var innerHTMLAsString = '',
+                    index,
+                    key,
+                    keys = [],
+                    len;
+
+                innerHTMLAsString += '<' + this.type;
+
+                if (!isEmptyObject(this.attributes)) {
+                    for (key in this.attributes) {
+                        if (this.attributes.hasOwnProperty(key)) {
+                            keys.push(key);
+                        }
+                    }
+
+                    keys.sort();
+                    len = keys.length;
+                    for (index = 0; index < len; index++) {
+                        innerHTMLAsString += ' ' + keys[index] + '="' + this.attributes[keys[index]] + '"';
+                    }
+                }
+
+                innerHTMLAsString += '>';
+
+                if (this.children.length > 0) {
+                    len = this.children.length;
+                    for (index = 0; index < len; index++) {
+                        if (isString(this.children[index])) {
+                            innerHTMLAsString += this.children[index];
+                        } else {
+                            innerHTMLAsString += this.children[index].innerHTML;
+                        }
+                    }
+                }
+                else if (this.content) {
+                    innerHTMLAsString += this.content;
+                }
+
+                innerHTMLAsString += '</' + this.type + '>';
+
+                return innerHTMLAsString;
             }
         };
         return domElement;
-    } ());
+    }());
     return domElement;
 }
 

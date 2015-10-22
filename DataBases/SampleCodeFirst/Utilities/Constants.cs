0bdababf37f2d7a00f7ab8f215774ddfa1e4aed3
pepinho24 <p_mil4ev@hotmail.com>
2015-10-22 23:52:11 +0300

    Added utilities project with RandomGenerator class.

---
+++ DataBases/SampleCodeFirst/Utilities/Constants.cs
@@ -0,0 +1,70 @@
+﻿namespace Utilities
+{
+    public static class Constants
+    {
+        public const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        public readonly static string[] Names = {
+            "Laurine","Storm",
+            "Bella","Salido",
+            "Leigh","Funari",
+            "Carmelo","Winsett",
+            "Linnie","Champlin",
+            "Robyn","Bolles",
+            "Maye","Rendell",
+            "Arielle","Matton",
+            "Aisha","Demille",
+            "Precious","Appelbaum",
+            "Callie","Wyble",
+            "Riley","Dostal",
+            "Ula","Valez",
+            "Kathline","Coots",
+            "Santiago","Mumm",
+            "Jean","Bartolome",
+            "Jeffrey","Kabel",
+            "Loan","Galland",
+            "Vern","Walker",
+            "Dudley","Cressey",
+            "Vita","Primmer",
+            "Weldon","Bribiesca",
+            "Marx","Amerman",
+            "Tomeka","Rackham",
+            "Danny","Han",
+            "Ethelene","Demmer",
+            "Dannette","Biery",
+            "Caitlyn","Seagle",
+            "Doretha","Cowherd",
+            "Geraldine","Knapp",
+            "Zackary","Helwig",
+            "Trinity","Wedge",
+            "Fannie","Strachan",
+            "Moshe","Zeno",
+            "Kendal","Son",
+            "Neil","Fendley",
+            "Versie","Fisher",
+            "Kassandra","Kiley",
+            "Odelia","Burrage",
+            "Celine","Rosenbloom",
+            "Jeni","Pia",
+            "Chas","Gaumer",
+            "Joyce","Hornak",
+            "Francesco","Carrier",
+            "Claudio","Rockefeller",
+            "Alease","Gallego",
+            "Lorilee","Buntin",
+            "Sherice","Mcneel",
+            "Lennie","Weston",
+            "Betsy","Stacker"
+        };
+
+        public static readonly string[] Cities =
+        {
+        "Sofia", "Plovdiv", "Varna", "Targovishte", "Velingrad", "Burgas",
+        "Popovo", "Karlovo", "Pavel Banya", "Trustenik", "Isperikh", "Shumen", "Orlovets", "Dobrich", "Gabrovo", "Haskovo", "Provadiya", "Kharmanli", "Sliven", "Stara Zagora", "Kozloduy", "Shabla", "Kavarna", "Sevlievo", "Lovech", "Gorna Oryakhovitsa", "Blagoevgrad", "Petrich", "Krumovgrad", "Krichim", "Smolyan", "Radnevo", "Muglizh", "Troyan", "Veliko Tarnovo", "Nikopol", "Kazanlak", "Chakalarovo", "Pernik", "Ruen", "Barutin", "Bratsigovo", "Isperikhovo", "Godech", "Chernolik", "Razgrad", "Dulovo", "Yambol", "Asenovgrad", "Pazardzhik", "Chirpan", "Rozino", "Ravda", "Montana", "General-Toshevo", "Aksakovo", "Baltchik", "Rodopi", "Ikhtiman", "Samokov", "Svoge", "Sopot", "Kardzhali", "Pleven", "Kyustendil", "Topoli", "Beloslav", "Dulgopol", "Obzor", "Cherven", "Vratsa", "Borovan", "Knezha", "Cherven Bryag", "Sandanski", "Dupnitsa", "Lozno", "Krupnik", "Boboshevo", "Rousse", "Sandrovo", "Zavet",
+        "Svishtov", "Belene", "Pavlikeni", "Samovodene", "Polski Trumbesh", "Kostievo", "Akhtopol", "Nesebar", "Sredets", "Pomorie", "Simitli", "Slivnitsa", "Mezdra", "Batak", "Elkhovo", "Belogradchik", "Tutrakan", "Botevgrad", "Moderno Predgradie", "Borovo", "Topolovo", "Bankya", "Konush", "Dimitrovgrad", "Smyadovo", "Gulubovo", "Simeonovgrad", "Panagyurishte", "Bansko", "Yakoruda", "Silistra", "Byala", "Choba", "Iskrets", "Vlado Trichkov", "Zlatitrap", "Nova Zagora", "Devin", "Gotse Delchev", "Dragizhevo", "Byala", "Momchilgrad", "Pravets", "Etropole", "Malo Konare",
+        "Aytos", "Bukovo", "Banite", "Chiprovtsi", "Mesta", "Tryavna", "Berkovitsa", "Novi Pazar", "Krivodol", "Elena", "Strazhitsa", "Peshtera", "Sozopol", "Moravka", "Kovachite", "Omurtag", "Vidin", "Kostinbrod", "Ardino", "Alfatar", "Rakovski", "Poleto", "Rila", "Draginovo", "Oryakhovitsa", "Dolna Oryakhovitsa", "Kostenets", "Raduil", "Bukovo", "Strelcha", "Sokolets", "Skravena", "Vrachesh", "Stamboliyski", "Lom", "Bistritsa", "Khisarya", "Shivachevo", "Anton", "Madan", "Elin Pelin", "Veliko", "Debelets", "Oryakhovets", "Markovo", "Bregare", "Byala Slatina", "Sofronievo", "Kula", "Lyulyakovo", "Kableshkovo", "Antonovo", "Belovo", "Dospat", "Dolna Banya", "Septemvri", "Bobovdol", "Zlatograd", "Tervel", "Lyaskovets", "Mikrevo", "Vievo", "Chernomorets", "Kran", "Karnobat", "Dryanovo", "Ovoshtnik", "Koprivets", "Vetren", "Lukovit", "Babovo", "Kamenovo", "Orizovo", "Svilengrad", "Rudozem", "Aleksandriya", "Rakitovo", "Ivaylovgrad", "Glogovo", "Gurkovo", "Marikostinovo", "Tochilari", "Slatina",
+        "Razlog", "Prespa", "Gurmazovo", "Novi Iskur", "Samuil", "Pchelishte", "Dragash Voyvoda", "Dolna Mitropoliya", "Koprivlen", "Suedinenie", "Kazashka Reka", "Ganchovo"
+        };
+
+    }
+}
---
+++ DataBases/SampleCodeFirst/Utilities/RandomGenerator.cs
@@ -0,0 +1,70 @@
+﻿namespace Utilities
+{
+    using System;
+
+    public class RandomGenerator
+    {
+        private static RandomGenerator instance;
+
+        private readonly Random random;
+
+        private RandomGenerator()
+        {
+            this.random = new Random();
+        }
+
+        public static RandomGenerator Instance
+        {
+            get
+            {
+                return instance ?? (instance = new RandomGenerator());
+            }
+        }
+
+        public int GetRandomNumber(int min, int max)
+        {
+            return this.random.Next(min, max + 1);
+        }
+
+        public string GetRandomString(int length)
+        {
+            var chars = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = Constants.Letters[this.GetRandomNumber(0, Constants.Letters.Length - 1)];
+            }
+
+            return new string(chars);
+        }
+
+        public string GetRandomFirstName()
+        {
+            // TODO: improve logic for not even number of names in the array
+            var randomEvenNumber = 2 * this.GetRandomNumber(0, (Constants.Names.Length - 1) / 2);
+            return Constants.Names[randomEvenNumber];
+        }
+
+        public string GetRandomLastName()
+        {
+            // TODO: improve logic for not even number of names in the array
+            var randomEvenNumber = 2 * this.GetRandomNumber(0, (Constants.Names.Length - 1) / 2);
+            return Constants.Names[randomEvenNumber + 1];
+        }
+
+        public string GetRandomCityName()
+        {
+            return Constants.Cities[this.GetRandomNumber(0, Constants.Cities.Length -1)];
+        }
+
+        public DateTime GetRandomDate(DateTime? startDate = null, DateTime? endDate = null)
+        {
+            DateTime start = startDate ?? new DateTime(1995, 1, 1);
+            DateTime end = endDate ?? new DateTime(2050, 12, 31);
+            Random gen = new Random();
+
+            int range = (end - start).Days;
+            return start.AddDays(this.GetRandomNumber(0, range - 1));
+        }
+    }
+}
